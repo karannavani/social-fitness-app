09d85fb2f2f587be85c3916cc1e4c56e0b247ab2
Tristan Hall <tristanalex1986@gmail.com>
2018-09-05 22:19:20 +0100

    added login and registration routes

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const Router = express.Router();
 
 const exerciseController = require('../controllers/exerciseController');
+const authController = require('../controllers/authController');
+
+Router.route('/login')
+  .post(authController.login);
+
+Router.route('/register')
+  .post(authController.register);
 
 Router.route('/exercises')
   .get(exerciseController.index);
