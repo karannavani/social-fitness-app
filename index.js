4595f05afd4c32e6b2499b33fc746d5a723bf4cc
Karan Navani <kknavani@gmail.com>
2018-09-05 21:21:36 +0100

    completed index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const Router = require('./config/routes');
+const errorHandler = require('./lib/errorHandler');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const { port, dbUri } = require('./config/environment');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(dbUri);
+
+app.use('/api', Router);
+app.use(errorHandler);
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.listen(port, () => console.log(`Express is running on port ${port}`));
+
+module.exports = app;
