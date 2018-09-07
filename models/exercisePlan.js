2cbb1ec6e215837c807eabf8519db7e228f93beb
Karan Navani <kknavani@gmail.com>
2018-09-07 09:43:15 +0100

    updated exercise plan

--- models/exercisePlan.js
+++ models/exercisePlan.js
@@ -2,59 +2,51 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const exercisePlanSchema = new mongoose.Schema({
+  dayOne: {
+    rest: {type: Boolean, default: false },
+    exerciseCompleted: { type: Boolean, default: null},
+    time: Number,
+    intensity: String
+  },
+  dayTwo: {
+    rest: {type: Boolean, default: false },
+    exerciseCompleted: { type: Boolean, default: null},
+    time: Number,
+    intensity: String
+  },
+  dayThree: {
+    rest: {type: Boolean, default: false },
+    exerciseCompleted: { type: Boolean, default: null},
+    time: Number,
+    intensity: String
+  },
+  dayFour: {
+    rest: {type: Boolean, default: false },
+    exerciseCompleted: { type: Boolean, default: null},
+    time: Number,
+    intensity: String
+  },
+  dayFive: {
+    rest: {type: Boolean, default: false },
+    exerciseCompleted: { type: Boolean, default: null},
+    time: Number,
+    intensity: String
+  },
+  daySix: {
+    rest: {type: Boolean, default: false },
+    exerciseCompleted: { type: Boolean, default: null},
+    time: Number,
+    intensity: String
+  },
+  daySeven: {
+    rest: {type: Boolean, default: false },
+    exerciseCompleted: { type: Boolean, default: null},
+    time: Number,
+    intensity: String
+  },
   user: { type: ObjectId, ref: 'User' }, //references the user model
   exercisePlanAdoptedFrom: { type: ObjectId, ref: 'ExercisePlan' }, //will have the program id
-  startDate: String,
-  day1: {
-    day: Number,
-    rest: {type: Boolean, default: false },
-    exerciseCompleted: { type: Boolean, default: null},
-    time: Number,
-    intensity: String
-  },
-  day2: {
-    day: Number,
-    rest: {type: Boolean, default: false },
-    exerciseCompleted: { type: Boolean, default: null},
-    time: Number,
-    intensity: String
-  },
-  day3: {
-    day: Number,
-    rest: {type: Boolean, default: false },
-    exerciseCompleted: { type: Boolean, default: null},
-    time: Number,
-    intensity: String
-  },
-  day4: {
-    day: Number,
-    rest: {type: Boolean, default: false },
-    exerciseCompleted: { type: Boolean, default: null},
-    time: Number,
-    intensity: String
-  },
-  day5: {
-    day: Number,
-    rest: {type: Boolean, default: false },
-    exerciseCompleted: { type: Boolean, default: null},
-    time: Number,
-    intensity: String
-  },
-  day6: {
-    day: Number,
-    rest: {type: Boolean, default: false },
-    exerciseCompleted: { type: Boolean, default: null},
-    time: Number,
-    intensity: String
-  },
-  day7: {
-    day: Number,
-    rest: {type: Boolean, default: false },
-    exerciseCompleted: { type: Boolean, default: null},
-    time: Number,
-    intensity: String
-  },
-  daysLogged: [String] //if the activity for that day has been logged, push the date into this array
+  startDate: Number
 }, { timestamps: true });
 
 // LIFECYCLE HOOKS
