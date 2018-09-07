f8ff916eb690711c2e378b177eefed8523248ac3
Tristan Hall <tristanalex1986@gmail.com>
2018-09-07 11:18:25 +0100

    leaderboard is working and page is divided out into divs for graphs

--- src/components/tribe/Tribe.js
+++ src/components/tribe/Tribe.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import _ from 'lodash';
+
 
 
 class TribeAside extends React.Component {
@@ -13,24 +15,39 @@ class TribeAside extends React.Component {
       .then(res => this.setState({ members: res.data} ));
   }
 
-  // const leaderBoardMembers = []
-  // this.state.members.sort()
+  leadersSort = () => {
+    const leaders = this.state.members;
+    console.log('Tribe members sorted by grit is', _.orderBy(leaders, ['grit', 'username'], 'desc'));
+    return _.orderBy(leaders, ['grit', 'username'], 'desc');
+  }
+
 
   render() {
     return (
       <div className="columns">
         <div className="column is-4" style={{ backgroundColor: 'green', height: '100vh' }}>
-          <div className="dashAsideFollowers" style={{ backgroundColor: 'white', height: '30vh', marginTop: '10px'}}>
+          <div className="dashAsideFollowers" style={{ backgroundColor: 'white', height: '20vh', marginTop: '10px'}}>
+            <p>YOUR TRIBE</p>
             {this.state.members && <p>{this.state.tribeName} has {this.state.members.length} members</p>}
           </div>
-          <div className="dashAsideLeaders" style={{ backgroundColor: 'white', height: '30vh', marginTop: '10px'}}>
-            {/* {this.state.members && this.state.members.sort()} */}
+          <div className="dashAsideLeaders" style={{ backgroundColor: 'white', height: '40vh', marginTop: '10px'}}>
+            <p>LEADER BOARD</p>
+            {this.state.members && this.leadersSort().slice(0, 10).map(leader =>
+              <div key={leader._id}>
+                {leader.username}
+              </div>
+            )}
           </div>
           <div className="dashAsidePrograms" style={{ backgroundColor: 'white', height: '30vh', marginTop: '10px'}}>
-
+            <p>ADOPTED PROGRAMMES</p>
           </div>
-          <div className="column is-8">
-
+        </div>
+        <div className="column is-8">
+          <div className="columns is-multiline">
+            <div className="column is-half" style={{ backgroundColor: 'red', height: '50vh', width: '50%'}}>this is a div</div>
+            <div className="column is-half" style={{ backgroundColor: 'yellow', height: '50vh', width: '50%'}}>this is a div</div>
+            <div className="column is-half" style={{ backgroundColor: 'blue', height: '50vh', width: '50%'}}>this is a div</div>
+            <div className="column is-half" style={{ backgroundColor: 'black', height: '50vh', width: '50%'}}>this is a div</div>
           </div>
         </div>
       </div>
