da608d6e904edd3ab36e27568ed6aafc12a97933
Tristan Hall <tristanalex1986@gmail.com>
2018-09-08 16:28:37 +0100

    registration form created and working

--- src/components/tribe/DataViz.js
+++ src/components/tribe/DataViz.js
@@ -22,21 +22,21 @@ class Graphs extends React.Component{
     return (
       <div className="columns is-multiline">
         <div className="column is-half whole-tribe-pie"
-          style={{ border: '1px solid black', height: '50vh', width: '50%'}}>
+          style={{ height: '50vh', width: '50%'}}>
           {this.state.user &&
             <TribeVsUserChart tribeGrit={this.tribeGrit()}
               userGrit={this.state.user.grit}/>}
         </div>
-        <div className="column is-half" style={{ border: '1px solid black', height: '50vh', width: '50%'}}>
+        <div className="column is-half" style={{ height: '50vh', width: '50%'}}>
           <p>User grit vs time since joining</p>
         </div>
-        <div className="column is-half" style={{ border: '1px solid black', height: '50vh', width: '50%'}}>
+        <div className="column is-half" style={{ height: '50vh', width: '50%'}}>
 
           <p>Tribe grit available vs achieved</p>
         </div>
-        <div className="column is-half" style={{ border: '1px solid black', height: '50vh', width: '50%'}}>
+        <div className="column is-half" style={{ height: '50vh', width: '50%'}}>
           <p>Time and intensity table</p>
-          
+
         </div>
       </div>);
   }
