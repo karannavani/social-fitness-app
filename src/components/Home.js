342fcee9da6971346be993cfccfe52ea4cd0f7df
Tristan Hall <tristanalex1986@gmail.com>
2018-09-08 22:47:39 +0100

    made changes to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return(
-    <div className="columns">
-      <div className="column is-multiline has-text-centered">
+    <div className="columns  is-multiline">
+      <div className="column has-text-centered is-multiline">
         <h1 className="title is-1">Welcome to Grit!</h1>
         <hr />
-        <p className="column is-full">Join a Tribe based on your exercise routines and earn Grit Points by taking on challenges, completing plans and showing that you have Grit.</p>
-        <p className="column is-full">You can track your progress and see how the rest of your Tribe are performing.</p>
-        <Link to='/login' className="column is-half button">Login</Link>
-        <Link to='/register' className="column is-half button">Sign Up</Link>
       </div>
+      <p className="column is-full has-text-centered">Join a Tribe based on your exercise routines and earn Grit Points by taking on challenges, completing plans and showing that you have Grit.</p>
+      <p className="column is-full has-text-centered">You can track your progress and see how the rest of your Tribe are performing.</p>
+      <Link to='/login' className="column is-2 is-offset-3 button is-info">Login</Link>
+      <Link to='/register' className="column is-2 is-offset-2 button is-info">Sign Up</Link>
     </div>
   );
 }
