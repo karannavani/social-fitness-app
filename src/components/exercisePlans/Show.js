7179489655e2c0edfc044482b96f8870ffdc6083
Sean.Rennie <sean.rennie6@gmail.com>
2018-09-08 12:47:35 +0100

    exercise show displays each days workout

--- src/components/exercisePlans/Show.js
+++ src/components/exercisePlans/Show.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 
+//components
+import UpcomingCard from '../common/cards/UpcomingCard';
+import RestCard from '../common/cards/RestCard';
+
 export default class ExercisePlanShow extends React.Component{
   state={};
 
@@ -9,19 +13,17 @@ export default class ExercisePlanShow extends React.Component{
       .then(res => this.setState({exercisePlan: res.data}));
   }
 
+  handleAdoption = () => {
+    
+  }
+
   render(){
     const { exercisePlan } = this.state;
     return(
       <section className='container'>
         {exercisePlan &&
-          //program name and tribe of creator
-          //average intensity and average workout time per day
-          //should include, all days laid out in similar format
-          //  --lift this from dash board for consistency
-          //if rest day then lift the rest day html from dashboard
-          //
           <div className='columns is-centered'>
-            <div className=' column is-6 columns is-multiline'>
+            <div className=' column is-8 columns is-mobile is-multiline'>
               <div className='column is-6'>
                 <h1 className='title is-5'>Plan Name</h1>
               </div>
@@ -29,36 +31,28 @@ export default class ExercisePlanShow extends React.Component{
                 <h1 className='title is-5'>Tribe</h1>
               </div>
               <div className='column is-6'>
-                <p><i className="fab fa-gripfire"></i>: {exercisePlan.intensityAvg}</p>
+                <p><i className="fas fa-fire fas-regular"></i>: {exercisePlan.intensityAvg}</p>
               </div>
               <div className='column is-6'>
-                <p><i className="far fa-clock"></i> Average: {exercisePlan.workoutTimeAvg}</p>
+                <p><i className="fas fa-stopwatch fas-regular"></i> Average: {exercisePlan.workoutTimeAvg}</p>
               </div>
 
+              {/* day cards */}
               <div className='column is-12'>
-                {/* {for(let i = 1; i < 8; i++){ */}
-                  <div className="card program-card">
-                    <div className="card-content">
-                      <h5 key="1" className="title is-5">Day {1}:</h5>
-                      <p>
-                        <span
-                          className="title is-5 upcomingDetails">
-                          <i className="fas fa-stopwatch fas-regular"></i>
-                          {exercisePlan[`day${1}`].time}
-                          mins
-                        </span>
-                        <span
-                          className="title is-5 upcomingDetails">
-                          <i className="fas fa-fire fas-regular"></i> {
-                            exercisePlan[`day${1}`].intensity}
-                        </span>
-                      </p>
-                    </div>
-                  </div>
-                {/* }} */}
+                {Object.keys(exercisePlan).map(key => {
+                  if(!exercisePlan[key].rest && exercisePlan[key].intensity){
+                    return <UpcomingCard key={key} title={key} programDetails={exercisePlan[key]} />;
 
+                  }else if(exercisePlan[key].rest){
+                    return  <RestCard key={key} programDay={key} title='Rest Day' />;
+                  }
+                })
+                }
               </div>
 
+              <div className='column is-12 has-text-centered'>
+                <button onClick={this.handleAdoption} className='button is-primary'>Adopt</button>
+              </div>
 
             </div>
           </div>
