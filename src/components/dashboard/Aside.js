e5e64709b6cbc790bd69ceb9dd5f2b09275bea10
Karan Navani <kknavani@gmail.com>
2018-09-09 03:35:30 +0100

    put the primary data in dashboard.js

--- src/components/dashboard/Aside.js
+++ src/components/dashboard/Aside.js
@@ -19,19 +19,23 @@ class Aside extends React.Component {
     unloggedDays: []
   }
   //
+  // componentDidMount() {
+  //   axios.get('/api/users')
+  //     .then(res => this.setState({ users: res.data[0], exerciseId: res.data[0].exercisePlan[0] },
+  //       () => this.getExercise()));
+  // }
+  //
+  // getExercise = () => { // sets the exercises from the current plan on the state
+  //   axios.get(`/api/exerciseplans/${this.state.exerciseId}`)
+  //     .then(res => this.setState({ exercises: res.data }, () => {
+  //       console.log('exercises are', this.state.exercises);
+  //       this.getProgram();
+  //       // this.props.shareExercises(this.state.exercises);
+  //     }
+  //     ));
+  // }
   componentDidMount() {
-    axios.get('/api/users/')
-      .then(res => this.setState({ users: res.data[0], exerciseId: res.data[0].exercisePlan[0] },
-        () => this.getExercise()));
-  }
-
-  getExercise = () => { // sets the exercises from the current plan on the state
-    axios.get(`/api/exerciseplans/${this.state.exerciseId}`)
-      .then(res => this.setState({ exercises: res.data }, () => {
-        console.log('exercises are', this.state.exercises);
-        this.getProgram();
-      }
-      ));
+    this.props.onRef(this);
   }
 
   handleEdit = ({ target: { name, value } }) => { // handles exercise edit for that day
@@ -46,7 +50,7 @@ class Aside extends React.Component {
     console.log('day is', day);
     if (id === 'complete') {
       this.setState({ editProgram: false });
-      axios.patch(`/api/exerciseplans/${this.state.exerciseId}`,
+      axios.patch(`/api/exerciseplans/${this.props.exerciseId}`,
         {[day.toLowerCase()]: this.state.programToday});
 
     } else if (id === 'skip') {
@@ -83,38 +87,42 @@ class Aside extends React.Component {
     }
   }
 
+  getExercises = () => {
+    this.setState({ exercises: this.props.exercises }, () => this.getProgram());
+  }
+
   getProgram = () => {
+    if (this.state.exercises) {
+      const { exercises: { startDate } } = this.state; // getting startDate of the exercise
+      // const today = moment.utc();
+      // const tomorrow = moment.utc().add(1, 'days');
+      const today = moment.utc().add(3, 'days'); //manual today for testing
+      const tomorrow = moment.utc(today).add(1, 'days');//manual tomorrowfor testing
 
-    const { exercises: { startDate } } = this.state; // getting startDate of the exercise
-    // const today = moment.utc();
-    // const tomorrow = moment.utc().add(1, 'days');
-    const today = moment.utc().add(3, 'days'); //manual today for testing
-    const tomorrow = moment.utc(today).add(1, 'days');//manual tomorrowfor testing
+      for (let i = 1; i < 8; i++) {
 
-    for (let i = 1; i < 8; i++) {
+        // generate 7 dates from the start date – these are the program dates
+        const date = moment.utc(moment.unix(startDate)).add(i-1, 'days');
+        // console.log('date is', date.format('DD/MM/YYYY'));
 
-      // generate 7 dates from the start date – these are the program dates
-      const date = moment.utc(moment.unix(startDate)).add(i-1, 'days');
-      // console.log('date is', date.format('DD/MM/YYYY'));
+        // if a program date matches today's date, get the program at that index and set it as today's program
+        if (date.format('DD/MM/YYYY') === today.format('DD/MM/YYYY')) {
+          const value = this.state.exercises[`day${i}`];
+          console.log('program for today is', value);
+          this.setState({ programToday: value, programDay: `Day ${i}`, rest: value.rest });
 
-      // if a program date matches today's date, get the program at that index and set it as today's program
-      if (date.format('DD/MM/YYYY') === today.format('DD/MM/YYYY')) {
-        const value = this.state.exercises[`day${i}`];
-        console.log('program for today is', value);
-        this.setState({ programToday: value, programDay: `Day ${i}`, rest: value.rest });
+          // saving the workout of the next day to the state
+        } else if (date.format('DD/MM/YYYY') === tomorrow.format('DD/MM/YYYY') ) {
+          const value = this.state.exercises[`day${i}`];
+          this.setState({ programTomorrow: value, tomorrowRest: value.rest });
 
-        // saving the workout of the next day to the state
-      } else if (date.format('DD/MM/YYYY') === tomorrow.format('DD/MM/YYYY') ) {
-        const value = this.state.exercises[`day${i}`];
-        this.setState({ programTomorrow: value, tomorrowRest: value.rest });
+        } else if(moment(date).isBefore(moment(today))) {
 
-      } else if(moment(date).isBefore(moment(today))) {
-
-        console.log(`${date.format('DD/MM/YYYY')} is before ${today.format('DD/MM/YYYY')}`);
-        this.checkUnlogged(this.state.exercises[`day${i}`], `Day ${i}`);
+          console.log(`${date.format('DD/MM/YYYY')} is before ${today.format('DD/MM/YYYY')}`);
+          this.checkUnlogged(this.state.exercises[`day${i}`], `Day ${i}`);
+        }
       }
     }
-
   }
 
   checkUnlogged = (exercise, i) => {
@@ -140,7 +148,7 @@ class Aside extends React.Component {
       exercises: {...this.state.exercises, [day.toLowerCase()]: newProgramState }
     }, () => console.log(this.state.exercises));
 
-    axios.patch(`/api/exerciseplans/${this.state.exerciseId}`, {[day.toLowerCase()]: newProgramState});
+    axios.patch(`/api/exerciseplans/${this.props.exerciseId}`, {[day.toLowerCase()]: newProgramState});
   }
   //
 
