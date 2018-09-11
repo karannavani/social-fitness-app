5ef09e29c92878c45f1a91f6685403dcfc7d73dd
Karan Navani <kknavani@gmail.com>
2018-09-11 15:38:36 +0100

    updated uri

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const dbUri = process.env.MONGODB_DBURI || 'mongodb://localhost:/social-fitness-app';
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:/social-fitness-app';
 // const env = process.env.NODE_ENV || 'dev';
 const port = process.env.PORT || 4000;
 
