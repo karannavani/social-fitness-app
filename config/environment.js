98d2e45516fca9ee8c581c3bc8f21781732c8784
Karan Navani <kknavani@gmail.com>
2018-09-11 14:47:32 +0100

    removed env from var names

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 const dbUri = process.env.MONGODB_DBURI || 'mongodb://localhost:/social-fitness-app';
-const env = process.env.NODE_ENV || 'dev';
+// const env = process.env.NODE_ENV || 'dev';
 const port = process.env.PORT || 4000;
 
 const secret = process.env.SECRET || 'GRIT';
 
-module.exports = { port, dbUri, secret, env };
+module.exports = { port, dbUri, secret };
