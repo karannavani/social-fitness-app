f6b051bb2b6ff7bd5b5bc3c23d10b7fa770e9ffe
Karan Navani <kknavani@gmail.com>
2018-09-11 14:44:18 +0100

    added env variable

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 const dbUri = process.env.MONGODB_DBURI || 'mongodb://localhost:/social-fitness-app';
+const env = process.env.NODE_ENV || 'dev';
 const port = process.env.PORT || 4000;
 
 const secret = process.env.SECRET || 'GRIT';
 
-module.exports = { port, dbUri, secret };
+module.exports = { port, dbUri, secret, env };
