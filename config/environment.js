234108adf951d23dfdf1db197098e6ff4294bdba
Karan Navani <kknavani@gmail.com>
2018-09-17 14:21:03 +0100

    updated

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT || 4000;
-const env = process.env.NODE_ENV || 'dev'; //production or dev
-const dbUri = process.env.MONGODB_URI || `mongodb://localhost/social-fitness-app-${env}`;
-// const dbUri = process.env.MONGODB_DBURI || 'mongodb://localhost:/social-fitness-app';
+// const env = process.env.NODE_ENV || 'dev'; //production or dev
+// const dbUri = process.env.MONGODB_URI || `mongodb://localhost/social-fitness-app-${env}`;
+const dbUri = process.env.MONGODB_DBURI || 'mongodb://localhost:/social-fitness-app';
 // heroku comment
 const secret = process.env.SECRET || 'GRIT';
 
