dd84fb12cfef53513decb275d9d8efb6f94ccbdd
Karan Navani <kknavani@gmail.com>
2018-09-17 13:58:18 +0100

    heroku comment

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev'; //production or dev
 const dbUri = process.env.MONGODB_URI || `mongodb://localhost/social-fitness-app-${env}`;
 // const dbUri = process.env.MONGODB_DBURI || 'mongodb://localhost:/social-fitness-app';
-
+// heroku comment
 const secret = process.env.SECRET || 'GRIT';
 
 module.exports = { port, dbUri, secret };
