dce6a3528016b348af8721e0978f72aff36e7dc1
Karan Navani <kknavani@gmail.com>
2018-09-18 14:07:59 +0100

    updated package json

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect(dbUri);
 
+app.use(express.static(`${__dirname}/public`));
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-app.use(express.static(`${__dirname}/public`));
 
 app.use('/api', Router);
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
